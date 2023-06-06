a90b5c1841ae17b0a3379205008cdd97f64f5fe0
DensityInfinite <douglas_jiang@hotmail.com>
2023-06-06 10:43:06 +1000

    Ported GUI from previous task

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import pygame, pygame_gui as pygui, json
+import pygame, json
 from sys import exit
 
 import resources.utils as utils
-from resources.game_settings import GameSettings
+import resources.game_settings as game_settings
+import resources.gui.gui as gui
 
 
 class Game:
@@ -11,31 +12,49 @@ class Game:
         pygame.font.init()
         self.clock = pygame.time.Clock()
         self.json_utils = utils.JsonUtils()
-        self.settings = GameSettings()
+        self.screen_settings = game_settings.Screen()
+        self.colours = game_settings.Colours()
+        self.dev_tools = game_settings.Dev()
+        self.gui = gui
 
         # Screen
         self.screen = pygame.display.set_mode(
-            self.settings.screen_size,
+            self.screen_settings.screen_size,
             pygame.HWSURFACE | pygame.SRCALPHA,
-            self.settings.vsync,
+            self.screen_settings.vsync,
         )
-        pygame.display.set_caption(self.settings.title)
-        self.manager = pygui.UIManager(self.settings.screen_size)
+        self.screen_width, self.screen_height = self.screen_settings.screen_size
+        self.screen_centre_pos = (self.screen_width // 2, self.screen_height // 2)
+        pygame.display.set_caption(self.screen_settings.title)
+
+        # Cursor
+        pygame.mouse.set_visible(not self.dev_tools.cursor_sprite_as_cursor)
+        self.cursor = pygame.sprite.GroupSingle()
+        self.cursor.add(gui.Cursor())
 
     def run_game(self) -> None:
+        gui_group = pygame.sprite.RenderUpdates()
+        hello_button = self.gui.Button(
+            pygame.Rect(self.screen_centre_pos, (300, 50)),
+            "Hello!",
+            self.colours.background_colour,
+        )
+        gui_group.add(hello_button)
         while 1:
-            time_delta = self.clock.tick(self.settings.fps)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     exit()
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    self.cursor.sprite.register_click(True)
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    self.cursor.sprite.register_click(False)
 
-                self.manager.process_events(event)
+            gui_group.update(self.cursor)
+            self.cursor.update(pygame.mouse.get_pos())
 
-            self.manager.update(time_delta)
-
-            self.screen.fill(self.settings.background_colour)
-            self.manager.draw_ui(self.screen)
+            self.screen.fill(self.colours.background_colour)
+            gui_group.draw(self.screen)
 
             pygame.display.update()
 
--- resources/game_settings.py
+++ resources/game_settings.py
@@ -1,13 +1,41 @@
-import pygame
+import pygame, os.path as path
 
 
-class GameSettings:
+class Screen:
     def __init__(self) -> None:
-        # Screen
         self.title = "DensityInfinite & Peilin - Tile-Based RPG"
         self.screen_size = (800, 600)
         self.vsync = 1
         self.fps = 60
 
-        # Colours
-        self.background_colour = pygame.Color(255, 255, 255)
+
+class GUI:
+    def __init__(self) -> None:
+        self.rounded_corner_radius = 5
+        self.inner_corner_decrement = 2
+
+
+class Fonts:
+    def __init__(self) -> None:
+        self.button_font_name = "optima bold"
+        self.button_font = pygame.Font(
+            pygame.font.match_font(self.button_font_name), 25
+        )
+
+
+class Colours:
+    def __init__(self) -> None:
+        self.white = pygame.Color(255, 255, 255)
+        self.blue = pygame.Color(70, 223, 253)
+        self.grey = pygame.Color(50, 50, 50)
+        self.black = pygame.Color(0, 0, 0)
+
+        self.background_colour = self.black
+        self.button_text_colour = self.white
+
+
+class Dev:
+    def __init__(self) -> None:
+        self.button_pressed_event = pygame.USEREVENT + 1
+        self.cursor_sprite_size = 5
+        self.cursor_sprite_as_cursor = False
