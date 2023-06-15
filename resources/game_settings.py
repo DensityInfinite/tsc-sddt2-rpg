03773c414fb3ab9766708737243d8254fe93a02e
Shyboy0499 <krisyu0911@gmail.com>
2023-06-15 17:41:46 +1000

    change player_movement add data into game_setting

--- resources/game_settings.py
+++ resources/game_settings.py
@@ -11,3 +11,22 @@ class GameSettings:
 
         # Colours
         self.background_colour = pygame.Color(255, 255, 255)
+
+class PlayerSettings:
+    def __init__(self) -> None:
+        # Player
+        self.speed = 6
+        self.initial_position = (0, 0)
+        self.colour = (255, 0, 0)
+
+
+class GridSettings:
+    def __init__(self) -> None:
+        # Grid
+        self.colour = pygame.Color(0, 0, 0)
+
+
+class TileSettings:
+    def __init__(self) -> None:
+        # Tile
+        self.size = 32
