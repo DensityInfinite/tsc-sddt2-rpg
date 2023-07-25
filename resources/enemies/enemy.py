1ea0d641d0395a7f15785d2eb431b69537ca159f
DensityInfinite <douglas_jiang@hotmail.com>
2023-07-25 15:03:35 +1000

    Added state shuffle

--- resources/enemies/enemy.py
+++ resources/enemies/enemy.py
@@ -1,4 +1,6 @@
-import pygame, resources.game_settings as settings
+import pygame
+from random import randint
+import resources.game_settings as settings
 
 
 class Enemy(pygame.sprite.Sprite):
@@ -6,6 +8,8 @@ class Enemy(pygame.sprite.Sprite):
         # Initialise
         pygame.sprite.Sprite.__init__(self)
         self.map_settings = settings.Map()
+
+        self.state = "alone"
 
         self.raw_pos = (
             in_game_pos[0] * self.map_settings.tile_size
@@ -19,5 +23,16 @@ class Enemy(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.center = self.raw_pos
 
-    def update(self):
-        pass
+    def update(self, player_pos: tuple[int, int]):
+        if self.state == "alone":
+            self.state = self._reshuffle_state()
+
+    def _reshuffle_state(self) -> str:
+        new_state = randint(1, 3)
+        match new_state:
+            case 1:
+                return "alone"
+            case 2:
+                return "moving"
+            case 3:
+                return "chasing"
