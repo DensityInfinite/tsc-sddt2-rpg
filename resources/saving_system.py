4b894db42cdf1a1240371ece1814c01a8bddd22b
Shyboy0499 <krisyu0911@gmail.com>
2023-08-08 17:25:52 +1000

    add function of load_inventory

--- resources/saving_system.py
+++ resources/saving_system.py
@@ -11,3 +11,20 @@ def save_inventory(inventory, file_name, overwrite=True):
     ]
     with open(file_name, "w") as file:
         json.dump(inventory_data, file)
+
+
+def load_inventory(file_name, Inventory, Item):
+    if not os.path.exists(file_name):
+        raise FileNotFoundError(f"File '{file_name}' not found.")
+
+    with open(file_name, "r") as file:
+        inventory_data = json.load(file)
+
+    inventory = Inventory()
+    for item_data in inventory_data:
+        if "name" not in item_data or "category" not in item_data:
+            raise ValueError("Invalid item data in JSON file.")
+        item = Item(item_data["name"], item_data["category"])
+        inventory.add_item(item)
+
+    return inventory
