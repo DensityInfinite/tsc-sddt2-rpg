ff45aeefd1ce6df984476bff9f7fedd12b1d021a
Shyboy0499 <krisyu0911@gmail.com>
2023-08-08 13:50:37 +1000

    delete the test code and fill in the functon select_item

--- resources/item.py
+++ resources/item.py
@@ -27,39 +27,17 @@ class Inventory:
                 self.items.remove(item)
                 break
 
-    def select_item(self, item_name):
-        pass
+
+def select_item(self, item_name):
+    for item in self.items:
+        if item.name == item_name:
+            self.selected_item = item_name
+            break
+    else:
+        print(f"Item '{item_name}' not found in the inventory.")
 
     def get_items(self) -> list:
         return self.items
 
     def get_holding_item(self) -> str:
         return self.selected_item
-
-    def display_items(self):
-        print("Inventory:")
-        for item in self.items:
-            print(f"{item.name} ({item.category})")
-
-
-# Items
-sword = Item("Sword", "Weapon")
-apple = Item("Apple", "Food")
-torch = Item("Torch", "Tool")
-
-# Inventory
-player_inventory = Inventory()
-
-# Picking up items
-player_inventory.add_item(sword)
-player_inventory.add_item(apple)
-player_inventory.add_item(torch)
-
-# Displaying items
-player_inventory.display_items()
-
-# Removing an item
-player_inventory.remove_item("Apple")
-
-# Displaying items after removing an item
-player_inventory.display_items()
