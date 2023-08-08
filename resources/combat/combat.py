b14e8da636e06f9dc5cf54a60fa04a1258a60b57
DensityInfinite <douglas_jiang@hotmail.com>
2023-08-08 14:57:32 +1000

    Gets player and enemy stats and udpates them every update

--- resources/combat/combat.py
+++ resources/combat/combat.py
@@ -1,6 +1,23 @@
 import pygame
+import game_settings
+from resources.player import Player
+from resources.enemies.enemy import Enemy
+from resources.items.item import Inventory
 
 
 class Combat:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, player: Player, enemy: Enemy, inventory: Inventory) -> None:
+        self.player = player
+        self.enemy = enemy
+
+        self.player_settings = game_settings.Player()
+        self.player_health, self.player_defence = self.player.get_stats()
+        self.player_base_damage = self.player_settings.base_damage
+        self.player_dmg_boost = (
+            15 if inventory.get_holding_item().get_category() == "weapon" else 0
+        )
+        self.enemy_health, self.enemy_defence = self.enemy.get_stats()
+
+    def update(self) -> None:
+        self.player_health, _ = self.player.get_stats()
+        self.enemy_health, _ = self.enemy.get_stats()
