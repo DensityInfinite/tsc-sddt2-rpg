47fad4df8804f8cc7c083542ef899bab1c141703
DensityInfinite <douglas_jiang@hotmail.com>
2023-08-15 16:50:15 +1000

    Map now load enemies

--- resources/map/map.py
+++ resources/map/map.py
@@ -1,5 +1,7 @@
-import pygame, os.path as path
+import pygame, os.path as path, ast
 import resources.game_settings as game_settings, resources.utils as utils
+
+from resources.enemies.enemy import Enemy
 
 
 class _TileTrigger(pygame.sprite.Sprite):
@@ -24,11 +26,12 @@ class Grid(pygame.sprite.Sprite):
         self.map_settings = game_settings.Map()
         self.json_utils = utils.JsonUtils()
 
-        self.image, self.triggers = self._init_grid(grid_id)
+        self.image, self.triggers, self.enemies = self._init_grid(grid_id)
 
-    def _init_grid(self, grid_id: int) -> tuple[pygame.Surface, list]:
+    def _init_grid(self, grid_id: int) -> tuple[pygame.Surface, list, list]:
         grid_image = pygame.Surface(self.screen_settings.screen_size)
         triggers = []
+        enemies = []
         grid_master: dict = self.json_utils.load_from_json(
             self.map_settings.grids_master_path
         )
@@ -38,6 +41,8 @@ class Grid(pygame.sprite.Sprite):
         )
         tiles: list = grid_file["tiles"]
         links: dict = grid_file["links"]
+        enemies_map: list = grid_file["enemies"]
+        enemies_data: list = grid_file["enemies_data"]
         textures = {
             " ": pygame.transform.scale(
                 pygame.image.load(
@@ -74,8 +79,21 @@ class Grid(pygame.sprite.Sprite):
                     triggers.append(_TileTrigger(cursor, "link", link=links[str(tile)]))
                 cursor = (cursor[0] + self.map_settings.tile_size, cursor[1])
             cursor = (0, cursor[1] + self.map_settings.tile_size)
+        cursor = (1, 1)
+        for row in enemies_map:
+            for tile in row:
+                if isinstance(tile, int):
+                    enemies.append(
+                        Enemy(
+                            cursor,
+                            ast.literal_eval(enemies_data[str(tile)]["movement"]), # type: ignore
+                            enemies_data[str(tile)]["speed"], # type: ignore
+                        )
+                    )
+                cursor = (cursor[0] + 1, cursor[1])
+            cursor = (1, cursor[1] + 1)
 
-        return grid_image, triggers
+        return grid_image, triggers, enemies
 
     def _get_type(self, tile_texture_name: str) -> str:
         match tile_texture_name:
