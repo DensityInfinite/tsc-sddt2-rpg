97e0d91432e83f80373c2f6d39dcd8ddf4df9375
Shyboy0499 <krisyu0911@gmail.com>
2023-08-17 10:15:47 +1000

    add the saving current place

--- resources/saving_system.py
+++ resources/saving_system.py
@@ -2,38 +2,42 @@ from resources.utils import JsonUtils
 import os
 
 
-def save_game_state(inventory, player_location, grid, file_name, overwrite=True):
-    if not overwrite and os.path.exists(file_name):
-        raise FileExistsError(f"File '{file_name}' already exists.")
+class SavingSystem:
+    def __init__(self):
+        self.json_utils = JsonUtils()
 
-    game_state = {
-        "inventory": [
-            {"name": item.name, "category": item.category} for item in inventory.items
-        ],
-        "player_location": player_location,
-        "grid": grid,
-    }
+    def save_game_state(
+        self, inventory, player_location, grid, file_name, overwrite=True
+    ):
+        if not overwrite and os.path.exists(file_name):
+            raise FileExistsError(f"File '{file_name}' already exists.")
 
-    json_utils = JsonUtils()  # Create an instance of JsonUtils
-    json_utils.save_to_json(file_name, game_state)  # Call the save_to_json method
+        game_state = {
+            "inventory": [
+                {"name": item.name, "category": item.category}
+                for item in inventory.items
+            ],
+            "player_location": player_location,  # Save player's current location
+            "grid": grid,  # Save the grid information
+        }
 
+        self.json_utils.save_to_json(file_name, game_state)
 
-def load_game_state(file_name, Inventory, Item):
-    if not os.path.exists(file_name):
-        raise FileNotFoundError(f"File '{file_name}' not found.")
+    def load_game_state(self, file_name, Inventory, Item):
+        if not os.path.exists(file_name):
+            raise FileNotFoundError(f"File '{file_name}' not found.")
 
-    json_utils = JsonUtils()  # Create an instance of JsonUtils
-    game_state = json_utils.load_from_json(file_name)  # Call the load_from_json method
+        game_state = self.json_utils.load_from_json(file_name)
 
-    inventory_data = game_state.get("inventory", [])
-    inventory = Inventory()
-    for item_data in inventory_data:
-        if "name" not in item_data or "category" not in item_data:
-            raise ValueError("Invalid item data in JSON file.")
-        item = Item(item_data["name"], item_data["category"])
-        inventory.add_item(item)
+        inventory_data = game_state.get("inventory", [])
+        inventory = Inventory()
+        for item_data in inventory_data:
+            if "name" not in item_data or "category" not in item_data:
+                raise ValueError("Invalid item data in JSON file.")
+            item = Item(item_data["name"], item_data["category"])
+            inventory.add_item(item)
 
-    player_location = game_state.get("player_location", None)
-    grid = game_state.get("grid", None)
+        player_location = game_state.get("player_location", None)
+        grid = game_state.get("grid", None)
 
-    return inventory, player_location, grid
+        return inventory, player_location, grid
